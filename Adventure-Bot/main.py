d25bc0b7e58f68c3c8b12c3dc70331859645d64c
Paul Frankovich <pmfrankovich@gmail.com>
2020-02-25 10:16:36 -0500

    Starting text adventure game bot

---
+++ Adventure-Bot/main.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+from discord.ext.commands.errors import *
+from discord.ext import commands
+import discord
+
+from aiohttp import ClientSession
+import datetime
+import asyncio
+
+from config import TOKEN
+
+initial_cogs = [
+    'cogs.commands'
+]
+
+class Adventure(commands.AutoShardedBot):
+    def __init__(self, **kwargs):
+        super().__init__(command_prefix=kwargs.pop('command_prefix', ('$.')), case_insensitive=True, **kwargs)
+        self.session = ClientSession(loop=self.loop)
+        self.start_time = datetime.datetime.utcnow()
+        self.clean_text = commands.clean_content(escape_markdown=True, fix_channel_mentions=True)
+
+    async def on_ready(self):
+        print(f'Successfully logged in as {self.user}\nSharded to {len(self.guilds)} guilds')
+
+        for ext in initial_cogs:
+            self.load_extension(ext)
+        print('Loaded Cogs')
+
+    @classmethod
+    async def setup(cls, **kwargs):
+        bot = cls()
+        try:
+            await bot.start(TOKEN, **kwargs)
+        except KeyboardInterrupt:
+            await bot.close()
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(Adventure.setup())
