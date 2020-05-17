d93963931adaca1dda71850d5a00062e2670e5bc
Paul Frankovich <pmfrankovich@gmail.com>
2020-05-17 12:40:26 -0400

    resync

--- Adventure-Bot/cogs/commands.py
+++ Adventure-Bot/cogs/commands.py
@@ -14,16 +14,33 @@ class Commands(commands.Cog):
         else:
             role = discord.utils.get(ctx.author.roles, name='Welfare')
             new_role = discord.utils.get(ctx.guild.roles, name='Bullets')
+        print(dir(ctx))
         await ctx.author.remove_roles(role)
         await ctx.author.add_roles(new_role)
 
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        if message.author.bot:
-            pass
-        else:
-            await message.channel.send(f'Hi, {message.author.mention}')
-            print(dir(message.author))
+    @commands.command()
+    async def reset_challenge(self, ctx):
+        """Reset Coding Events"""
+        role = discord.utils.get(ctx.guild.roles, name='OurRoleNameHere')
+        for member in role.members:
+            await member.remove_roles(role, reason='Reset event', atomic=True)
+
+    @bot.event
+    async def on_reaction_add():
+        print("Reaction added")
+        await print("reaction added")
+        
+    # @commands.Cog.listener()
+    # async def on_message(self, message):
+    #     if message.author.bot:
+    #         pass
+    #     else:
+    #         await message.channel.send(f'Hi, {message.author.mention}')
+    #         print(dir(message.author))
+
+    @commands.command(name='character')
+    async def character(self, ctx):
+        print(dir(ctx))
 
 def setup(bot):
     bot.add_cog(Commands(bot))
